4209464d373924cfea6add5ed441ee527053daf4
Forrest Verellen <fsverellen@mail.com>
2023-07-22 16:27:54 -0400

    first

---
+++ Circle Objects/Circle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+	internal class Circle
+	{
+		//Properties
+		private double Radius { get; set; }
+
+		//Constructor
+		public Circle(double _radius)
+		{
+			Radius = _radius;
+		}
+		//Methods
+		public double CalculateDiameter(double diameter)
+		{
+			diameter = Radius * 2;
+			return diameter;
+		}
+		public double CalculateCircumference(double circumference)
+		{
+			circumference = 2 * Math.PI * Radius;
+			return circumference;
+		}
+		public double CalculateArea(double area)
+		{
+			area = Math.PI * Radius * Radius;
+			return area;
+		}
+		public void Grow()
+		{
+			Radius = Radius * 2;
+		}
+		public double GetRadius()
+		{
+			return Radius;
+		}
+	}
+}
+
---
+++ Circle Objects/Program.cs
@@ -0,0 +1,45 @@
+﻿using Circle_Objects;
+
+
+    bool runProgram = true;
+    Console.WriteLine("Welcome to the Circle Tester");
+    Console.WriteLine("Enter radius");
+while (true)
+{
+    try
+    {
+        double input = double.Parse(Console.ReadLine().Trim().ToLower());
+        Circle myRadius = new Circle(input);
+        Console.WriteLine(myRadius.CalculateDiameter(input));
+        Console.WriteLine(myRadius.CalculateCircumference(input));
+        Console.WriteLine(myRadius.CalculateArea(input));
+        while (runProgram = true)
+        {
+            Console.WriteLine("Should the circle grow? (y/n)");
+            string input2 = Console.ReadLine().Trim().ToLower();
+            if (input2 == "y")
+            {
+                Console.WriteLine("The circle is magically growing");
+                myRadius.Grow();
+                myRadius.GetRadius();
+                Console.WriteLine(myRadius.CalculateDiameter(input));
+                Console.WriteLine(myRadius.CalculateCircumference(input));
+                Console.WriteLine(myRadius.CalculateArea(input));
+            }
+            else if (input2 == "n")
+            {
+                Console.WriteLine($"Goodbye. The circle's final radius is {myRadius.GetRadius()}");
+                runProgram = false;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input. Please try again");
+            }
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Not a valid number. Please try again");
+    }
+    }
